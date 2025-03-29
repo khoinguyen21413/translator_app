62fae01e06878878c3c1b016053db80b4bae88fd
khoinguyen21413 <khoinguyen.21413@gmail.com>
2025-03-29 14:37:10 +0700

    add: init root windown

---
+++ gui.py
@@ -0,0 +1,9 @@
+import tkinter as tk
+
+
+class TransalatorApp:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("Ứng dụng dịch ngôn ngữ - khoi nguyen")
+        self.root.geometry("600x400")
+        self.root.configure(bg="#F5E6C8")
---
+++ main.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from gui import TransalatorApp
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    TransalatorApp(root)
+    root.mainloop()
