123aed5ecc2515d46da76a080d5f55333a353e5a
khoinguyen21413 <khoinguyen.21413@gmail.com>
2025-03-29 14:45:55 +0700

    add: image for window

--- gui.py
+++ gui.py
@@ -1,9 +1,17 @@
 import tkinter as tk
+from PIL import Image, ImageTk
 
 
 class TransalatorApp:
     def __init__(self, root):
         self.root = root
         self.root.title("Ứng dụng dịch ngôn ngữ - khoi nguyen")
-        self.root.geometry("600x400")
-        self.root.configure(bg="#F5E6C8")
+        self.root.geometry("700x406")
+        self.root.configure(bg="#FDF6E6")
+
+        # Ảnh minh họa
+        img = Image.open("translator_app/assets/transalator_image_1.jpg")
+        img = img.resize((180, 114), Image.LANCZOS)
+        img_tk = ImageTk.PhotoImage(img)
+        img_label = tk.Label(root, image=img_tk, bg="#FDF6E6")
+        img_label.pack()
